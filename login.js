f4f962415850a239074fd1bb35fe87cc84dbd365
Tech-bot-alt <paul.tvog@hotmail.com>
2025-05-25 11:26:43 +0200

    Create login.js

---
+++ login.js
@@ -0,0 +1,10 @@
+const clientId = "Ov23liv6GxlwM7ONu1qb"; // Vul hier je eigen Client ID in
+const redirectUri = "https://tech-bot-alt.github.io/Het-Geld-Spel/auth/callback"; // Of het pad dat je in de OAuth-app hebt ingesteld
+
+function loginWithGitHub() {
+  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}`;
+  window.location.href = githubAuthUrl;
+}
+
+// Bijvoorbeeld, aan een knop koppelen:
+document.getElementById('github-login-btn').addEventListener('click', loginWithGitHub);
